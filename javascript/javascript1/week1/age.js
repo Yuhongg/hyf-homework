9a216ac155b15a2f1fb8736c0d49c1135837cc96
Yuhong.Yang <ganicesyang@sina.com>
2021-12-07 23:15:13 +0100

    Implemented task 3

--- javascript/javascript1/week1/age.js
+++ javascript/javascript1/week1/age.js
@@ -15,3 +15,18 @@ if (shouldShowResultInDogYears == true) {
     console.log("Your dog will be " + age + "years old in" + dogYearFuture);
 }
 
+
+let width;
+let depth;
+let height;
+let volumeInMeters = width * depth * height;
+let gardenSizeInM2;
+let fairHousePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+let actualHousePrice;
+
+if (actualHousePrice >= fairHousePrice) {
+    console.log("The prospective buyers are paying too much for the house.");
+} else {
+    console.log("The prospective buyers are paying a fair amount for the house.");
+}
+
