83160e9341c498d5ae4f8e1a2515710c55ae9c1c
Yuhong.Yang <ganicesyang@sina.com>
2021-12-15 10:59:41 +0100

    Committing changes to script file

--- javascript/javascript1/week1/age.js
+++ javascript/javascript1/week1/age.js
@@ -1,20 +1,18 @@
 let yearOfBirth = 1995;
 let yearFuture = 2027;
 let age = yearOfBirth - yearFuture;
-console.log("You will be" + age + "years old in" + yearFuture + ".");
+console.log("You will be " + age + "years old in " + yearFuture + ".");
 
-
-let dogYearOfBirth;
-let dogYearFuture;
+let dogYearOfBirth = 2010;
+let dogYearFuture = 2021;
 let dogYear = dogYearFuture - dogYearOfBirth;
-const shouldShowResultInDogYears;
+const shouldShowResultInDogYears = true;
 
 if (shouldShowResultInDogYears == true) {
-    console.log("Your dog will be " + dogYear + "years old in" + dogYearFuture);
+  console.log("Your dog will be " + dogYear + "years old in" + dogYearFuture);
 } else {
-    console.log("Your dog will be " + age + "years old in" + dogYearFuture);
+  console.log("Your dog will be " + age + "years old in" + dogYearFuture);
 }
-
 
 let width;
 let depth;
@@ -25,13 +23,47 @@ let fairHousePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 let actualHousePrice;
 
 if (actualHousePrice >= fairHousePrice) {
-    console.log("The prospective buyers are paying too much for the house.");
+  console.log("The prospective buyers are paying too much for the house.");
 } else {
-    console.log("The prospective buyers are paying a fair amount for the house.");
+  console.log("The prospective buyers are paying a fair amount for the house.");
 }
 
-let firstWords = ["great", "nice", "super", "neat", "urban", "time", "digital", "zoom", "goody", "future"];
-let secondWords = ["corporation", "firm", "company", "studio", "zone", "game", "easy", "vision", "mission", "door"];
+let firstWords = [
+  "great",
+  "nice",
+  "super",
+  "neat",
+  "urban",
+  "time",
+  "digital",
+  "zoom",
+  "goody",
+  "future",
+];
+let secondWords = [
+  "corporation",
+  "firm",
+  "company",
+  "studio",
+  "zone",
+  "game",
+  "easy",
+  "vision",
+  "mission",
+  "door",
+];
 let startupName;
 
-console.log("The startup '" + firstWords[4] + "" + secondWords[7] + "' contains " + (firstWords[4].length() + secondWords[7].length()) + "characters.")
+const randomNumber = Math.floor(Math.random() * 10);
+let firstWord = firstWords[randomNumber];
+let secondWord = secondWords[randomNumber];
+
+console.log(
+  "The startup '" +
+    firstWords +
+    "" +
+    secondWord +
+    "' contains " +
+    (firstWord.length + secondWord.length) +
+    "characters."
+);
