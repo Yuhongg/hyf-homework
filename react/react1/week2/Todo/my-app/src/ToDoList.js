7ffab87d833a1d51e6f5ebbb89e2aedf29dd3d66
Yuhongg <42935942+Yuhongg@users.noreply.github.com>
2022-05-25 16:26:05 +0200

    React1 week2 HM

--- react/react1/week2/Todo/my-app/src/ToDoList.js
+++ react/react1/week2/Todo/my-app/src/ToDoList.js
@@ -1,12 +1,21 @@
 import React from "react";
-import ToDo from "./Todo";
+import ToDo from "./ToDo";
 
-const ToDoList = ({ toDoList }) => {
+const ToDoList = ({ toDoList, handleToggle, handleFilter }) => {
   return (
     <div>
       {toDoList.map((todo) => {
-        return <ToDo todo={todo} />;
+        return (
+          <ToDo
+            todo={todo}
+            handleToggle={handleToggle}
+            handleFilter={handleFilter}
+          />
+        );
       })}
+      <button style={{ margin: "20px" }} onClick={handleFilter}>
+        Clear Completed
+      </button>
     </div>
   );
 };
